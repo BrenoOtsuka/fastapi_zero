96bea1b411077647b89ec62e071f4a02ce21d615
BrenoOtsuka <breno.otsuka@gmail.com>
2024-10-16 19:07:39 -0300

    added created_at and updated_at columns in todos table

--- fast_zero/settings.py
+++ fast_zero/settings.py
@@ -8,6 +8,3 @@ class Settings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     DATABASE_URL: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
